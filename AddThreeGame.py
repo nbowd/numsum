dae806d0e1e7cfc8b247dc8e076c338754119f36
Nick Bowden <nbowdenknows@gmail.com>
2020-12-12 13:53:42 -0800

    Meshed GUI and Numsum, before class refactor

--- AddThreeGame.py
+++ AddThreeGame.py
@@ -12,7 +12,6 @@ class AddThreeGame:
         self._moves = {
             'first': [],
             'second': [],
-            'choices': list(range(1, 10))
         }
         self._current_state = "UNFINISHED"
         self._current_turn = 'first'
@@ -23,16 +22,13 @@ class AddThreeGame:
         """Returns current state of the game"""
         return self._current_state
 
-    def get_current_moves(self):
-        """Returns current state of the game"""
-        return self._moves['choices']
-
     def get_current_turn(self):
         """Returns current state of the game"""
         return self._current_turn
 
-    def check_sums(self, player):
+    def check_sums(self):
         """This function checks for sums of 15 using a three pointer array technique."""
+        player = self.get_current_turn()
         for index in range(0, len(self._moves[player])):  # main pointer to be iterated over, starts at 0
             # By iteration over the main pointer, a two pointer technique can be used to check the other two values.
             left = index + 1  # left pointer starts one position to the right of the main pointer and increments
@@ -59,64 +55,46 @@ class AddThreeGame:
         elif player == 'second':
             self._current_turn = 'first'
 
-    def check_move(self, move):
+    def make_move(self, move):
+        """!"""
         if self._current_state == "UNFINISHED":
-            if player == self._current_turn:  # If correct players turn
-                if move in self._moves['choices'] and move in range(1, 10):  # Move is available (on the list, in range)
-                    return "Available"
-                else:  # Move is not available
-                    return "Unavailable"
-            else:  # incorrect players turn
-                return "Unavailable"
-        else:  # Game is finished
-            return "Unavailable"
-
-    def make_move(self, player, move):
-        """Checks if game is unfinished, if move is available and in the correct range, and if all the moves have been
-        used. If those pass then the move is recorded to the player that chose it and removed from remaining choices."""
-        if self._current_state == "UNFINISHED":
-            if player == self._current_turn:  # If correct players turn
-                if move in self._moves['choices'] and move in range(1, 10):  # Move is available (on the list, in range)
-                    self._moves[player].append(move)
-                    self._moves[player].sort()  # sorts the lists for the three point array check
-                    self._moves['choices'].remove(move)  # Choice has been removed from list
-                else:  # Move is not available
-                    return False
-                self.check_sums(player)  # ends loop and game here if player reaches sum of 15
-                if len(self._moves['choices']) == 0:  # Checks if the choice list is empty
-                    self._current_state = "DRAW!"
-                self.turn_change(player)
-                return True
-            else:  # incorrect players turn
-                return False
+            player = self.get_current_turn()
+            self._moves[player].append(move)
+            self._moves[player].sort()  # sorts the lists for the three point array check
+            self.check_sums()  # ends loop and game here if player reaches sum of 15
+            game_length = len(self._moves['first']) + len(self._moves['second'])
+            if game_length == 9:  # Checks if all the numbers have been chosen.
+                self._current_state = "DRAW!"
+            self.turn_change(player)
+            return True
         else:  # Game is finished
             return False
 
 
-print("\nWelcome to Numsum!\n")
-print("The goal is to get exactly 3 numbers that add up to 15.")
-print("When a number is chosen by one player, it becomes unavailable to the other player.\n")
-print("Good Luck!\n")
+# print("\nWelcome to Numsum!\n")
+# print("The goal is to get exactly 3 numbers that add up to 15.")
+# print("When a number is chosen by one player, it becomes unavailable to the other player.\n")
+# print("Good Luck!\n")
 
-while True:
-    game = AddThreeGame()
-    moves = game.get_current_moves()
-    while game.get_current_state() == 'UNFINISHED':
-        if game.get_current_turn() == 'first':
-            move = int(input(f"\nPlayer 1, select a number {moves}: "))
-            player = 'first'
-        elif game.get_current_turn() == 'second':
-            move = int(input(f"\nPlayer 2, select a number {moves}: "))
-            player = 'second'
-        if game.check_move(move) == 'Available':
-            game.make_move(player, move)
-        elif game.check_move(move) == 'Unavailable':
-            print("Invalid move, please select again.")
-    print("\n" + game.get_current_state())
-    if game.get_current_state() != 'DRAW!':
-        print(f"Winning numbers: {game.winning_numbers}")
-    replay = input("\nPlay again?[y/n] ")
-    if replay.lower() == 'y':
-        continue
-    else:
-        break
+
+# game = AddThreeGame()
+# moves = game.get_current_moves()
+# while game.get_current_state() == 'UNFINISHED':
+#     if game.get_current_turn() == 'first':
+#         move = int(input(f"\nPlayer 1, select a number {moves}: "))
+#         player = 'first'
+#     elif game.get_current_turn() == 'second':
+#         move = int(input(f"\nPlayer 2, select a number {moves}: "))
+#         player = 'second'
+#     if game.check_move(move) == 'Available':
+#         game.make_move(player, move)
+#     elif game.check_move(move) == 'Unavailable':
+#         print("Invalid move, please select again.")
+# print("\n" + game.get_current_state())
+# if game.get_current_state() != 'DRAW!':
+#     print(f"Winning numbers: {game.winning_numbers}")
+# replay = input("\nPlay again?[y/n] ")
+# if replay.lower() == 'y':
+#     continue
+# else:
+#     break
--- numsum_gui.py
+++ numsum_gui.py
@@ -1,41 +1,95 @@
 from tkinter import *
+import tkinter.font as font
+from tkmacosx import Button
 from math import floor
+from AddThreeGame import AddThreeGame
+
 
 HEIGHT = 700
 WIDTH = 800
+game = AddThreeGame()
+text_color = '#57D9A3'
+button_color = '#3B3B3B'
+background_color = '#222222'
+
 root = Tk()
+root.title("Numsum")
+root.resizable(width=False, height=False)
+
+myFont = font.Font(size=30)
 
 # This is where the text and other game stats will be displayed
-canvas = Canvas(root, height=HEIGHT, width=WIDTH)
+canvas = Canvas(root, height=HEIGHT, width=WIDTH, bg=background_color)
 canvas.pack()
 
 # A separate container inside the canvas to place the buttons
-frame = Frame(root, bg='#80c1ff')
-frame.place(relx=0.2, relwidth=0.6, relheight=0.6)  # 60% of the screen size
+frame = Frame(root)
+frame.place(relx=0.2, rely=0.192, relwidth=0.6, relheight=0.6)  # 60% of the screen size
 
 # Expands rows and columns of the frame's grid for widget resizing, without this the buttons stay small.
 Grid.rowconfigure(frame, [0, 1, 2], weight=1)
 Grid.columnconfigure(frame, [0, 1, 2], weight=1)
 
+player_one_label = Label(canvas, text='Player\n 1', font=['Silom', 40], bg=background_color, fg=text_color)  # Player 1 label
+player_one_label.place(relx=0.01, rely=0.02)
 
-def button_click(index, n):
+player_two_label = Label(canvas, text='Player\n 2', font=['Silom', 40], bg=background_color, fg=text_color)  # Player 2 label
+player_two_label.place(relx=0.80, rely=0.02)
+
+canvas.create_line(0, 131, 800, 131, width=5, fill=text_color)  # Line that separates number pad from player labels
+
+def button_click(index, button_number):
     # Disable the button by index
-    buttons[index].config(state="disabled")
-    return n
+    buttons[index].config(state="disabled", bg=background_color)
+    print(button_number)
+    game.make_move(button_number)
+    print(game.get_current_turn())
+    check_game_end()
+
+def check_game_end():
+    if game.get_current_state() != 'UNFINISHED':  # Game has a winner or is a draw
+        root.title(game.get_current_state())  # Displays in title bar
+        for x in range(len(buttons)):  # Disables the remaining buttons
+            buttons[x].config(state="disabled", bg=background_color)
+        if game.get_current_state() == 'FIRST PLAYER WON!!!':  # Game has a winner, Display WINNER! with astrisk on left or right.
+            win_label = Label(canvas, text='*WINNER', font=['Silom', 80], bg=background_color, fg=text_color,
+                               justify='center')
+            win_label.place(relx=0.23, rely=0.02)
+            check_replay()
+        elif game.get_current_state() == 'SECOND PLAYER WON!!!':  # Game has a winner, Display WINNER! with astrisk on left or right.
+            win_label = Label(canvas, text='WINNER*', font=['Silom', 80], bg=background_color, fg=text_color,
+                               justify='center')
+            win_label.place(relx=0.28, rely=0.02)
+        elif game.get_current_state() == 'DRAW!':  # Game was a draw, display DRAW! in text
+            draw_label = Label(canvas, text='DRAW!', font=['Silom', 80], bg=background_color, fg=text_color, justify='center')
+            draw_label.place(relx=0.32, rely=0.02)
+
+def check_replay():
+    play_again_label = Label(canvas, text='Play Again?', font=['Silom',20], bg=background_color, fg=text_color, justify='center')
+    play_again_label.place(relx=0.418,rely=0.8)
+    yes_button = Button(canvas, bg=button_color, fg=text_color, text='Yes', width=80, height=60)
+    no_button = Button(canvas, bg=button_color, fg=text_color, text='No', width=80, height=60)
+    yes_button.place(relx=.395, rely=0.85)
+    no_button.place(relx=0.5, rely=0.85)
+
 
 # A collection (list) to hold the references to the buttons created below
 buttons = []
 
 for index in range(1, 10):
-    n = index
+    button_number = index
     gr = index - 1
-    button = Button(frame, bg="White", text=n, relief=GROOVE,
-                    command=lambda index=index - 1, n=n: button_click(index, n))
-
+    #  The index-1 accounts for the diff between the buttons numbers starting at 1 and the list indices starting at 0.
+    button = Button(frame, bg=button_color, fg=text_color, text=button_number, command=lambda index=index - 1, button_number=button_number: button_click(index, button_number))
+    button['font'] = myFont
     # Add the button to the window
     button.grid(row=floor((gr / 3)), column=(gr % 3), sticky="nsew")
-
     # Add a reference to the button to 'buttons'
     buttons.append(button)
-    print(gr)
+
+
+if game.get_current_state() == 'UNFINISHED':
+    print(game.get_current_turn())
+    print(font.families())
+
 root.mainloop()
